0544562eb967262d0a47e6095d33fdb754c7f03f
Njabulo Jb <fanajbai@gmail.com>
2025-10-29 21:25:50 +0200

    Create vcard.js

---
+++ commandes/vcard.js
@@ -0,0 +1,33 @@
+const { fana } = require("../njabulo/fana");
+const { getAllSudoNumbers, isSudoTableNotEmpty } = require("../bdd/sudo");
+const conf = require("../set");
+
+fana({ 
+  nomCom: "vcard", 
+  aliases: ["card", "contact"], 
+  categorie: "vcard", 
+  reaction: "📇" 
+}, async (dest, zk, commandeOptions) => {
+  const { repondre, ms, arg } = commandeOptions;
+  try {
+    const quotedMessage = ms.quoted ? ms.quoted : ms;
+    const quotedSender = quotedMessage.sender;
+    if (!quotedSender) {
+      return repondre('Reply to a user\'s message to save their number.');
+    }
+    if (!arg[0]) {
+      return repondre('Please provide a name for the contact.');
+    }
+    const name = arg.join(' ');
+    const phoneNumber = quotedSender.split('@')[0].replace(/[^0-9]/g, '');
+    const vcardString = `BEGIN:VCARD\n` + `VERSION:3.0\n` + `FN:${name}\n` + `TEL;type=CELL;type=VOICE;waid=${phoneNumber}:${phoneNumber}\n` + `END:VCARD`;
+    zk.sendMessage(dest, {
+      contacts: {
+        displayName: name,
+        contacts: [{ displayName: name, vcard: vcardString }]
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
