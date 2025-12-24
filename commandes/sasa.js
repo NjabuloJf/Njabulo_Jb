0a447c98bad2ca11920402046937136bd6a1e764
Njabulo Jb <fanajb65@gmail.com>
2025-12-24 23:13:07 +0200

    Create sasa.js

---
+++ commandes/sasa.js
@@ -0,0 +1,88 @@
+
+
+const { fana } = require("../njabulo/fana");
+const config = require("../set");
+const { generateWAMessageContent, generateWAMessageFromContent } = require('@whiskeysockets/baileys');
+
+fana({ 
+  nomCom: "sasa", 
+  alias: ["speed", "pong"], 
+  categorie: "General", 
+  reaction: "📌", 
+  use: ".ping", 
+}, async (dest, zk, commandeOptions) => {
+  console.log('Command triggered!');
+  const { repondre, ms } = commandeOptions;
+  try {
+    const start = new Date().getTime();
+    const reactionEmojis = ['❄️'];
+    const textEmojis = ['🚀'];
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    await zk.sendMessage(dest, { react: { text: textEmoji, key: ms.key } });
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+    const imageUrl = "https://files.catbox.moe/u6v5ir.jpg";
+    const card = {
+      header: {
+        title: `⏳ *PING* : *${responseTime.toFixed(2)}s`,
+        hasMediaAttachment: true,
+        imageMessage: (await generateWAMessageContent({ image: { url: imageUrl } }, { upload: zk.waUploadToServer })).imageMessage,
+      },
+      body: {
+        text: `⏳ *PING* : *${responseTime.toFixed(2)}s ${reactionEmoji}* `,
+      },
+      footer: {
+        text: "🔹 Play song",
+      },
+      nativeFlowMessage: {
+        buttons: [
+          {
+            name: "cta_url",
+            buttonParamsJson: JSON.stringify({
+              display_text: "🌐 View on YouTube",
+              url: `https://youtu.be/`,
+            }),
+          },
+          {
+            name: "cta_copy",
+            buttonParamsJson: JSON.stringify({
+              display_text: "📋 Copy Link",
+              copy_code: "https://youtu.be/",
+            }),
+          },
+        ],
+      },
+    };
+
+    const message = generateWAMessageFromContent(
+      dest,
+      {
+        viewOnceMessage: {
+          message: {
+            messageContextInfo: {
+              deviceListMetadata: {},
+              deviceListMetadataVersion: 2,
+            },
+            interactiveMessage: {
+              header: { text: `🔍 Search Results for` },
+              body: { text: `📂 Found 1 result` },
+              carouselMessage: { cards: [card] },
+            },
+          },
+        },
+      },
+      { quoted: ms }
+    );
+
+    await zk.relayMessage(dest, message.message, { messageId: message.key.id });
+  } catch (e) {
+    console.error("Error in ping command:", e);
+    repondre(`An error occurred: ${e.message}`);
+  }
+});
+
