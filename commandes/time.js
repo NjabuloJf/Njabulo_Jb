5651f90fc6d88f8fc60dd8ef563ed4387359d07a
Njabulo Jb <fanajb65@gmail.com>
2025-12-25 09:18:49 +0200

    Update time.js

--- commandes/time.js
+++ commandes/time.js
@@ -22,6 +22,20 @@ fana({
     while (textEmoji === reactionEmoji) {
       textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
     }
+    
+   const runtime = function (seconds) {
+  seconds = Number(seconds);
+  var d = Math.floor(seconds / (3600 * 24));
+  var h = Math.floor((seconds % (3600 * 24)) / 3600);
+  var m = Math.floor((seconds % 3600) / 60);
+  var s = Math.floor(seconds % 60);
+  var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+};
+
 
     await zk.sendMessage(dest, { react: { text: textEmoji, key: ms.key } });
     const end = new Date().getTime();
@@ -44,7 +58,7 @@ const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
         imageMessage: (await generateWAMessageContent({ image: { url: randomNjabulourl } }, { upload: zk.waUploadToServer })).imageMessage,
       },
       body: {
-        text: `⏳ *ᴘɪɴɢ* : *${responseTime.toFixed(2)}s ${reactionEmoji}* `,
+        text: `⏳ *ᴘɪɴɢ* : *${runtime(process.uptime())}  ${reactionEmoji}* `,
       },
       footer: {
         text: "Pσɯҽɾ Ⴆყ Ɲנαвυʟσ Jbᯤ",
