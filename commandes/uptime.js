8d6123cff9eb627145c61b4b49c2b1977b9164c8
Njabulo Jb <fanajb65@gmail.com>
2025-12-26 17:15:42 +0200

    Create uptime.js

---
+++ commandes/uptime.js
@@ -0,0 +1,147 @@
+
+
+const { fana } = require("../njabulo/fana");
+const config = require("../set");
+const { generateWAMessageContent, generateWAMessageFromContent } = require('@whiskeysockets/baileys');
+
+fana({
+  nomCom: "uptime",
+  alias: ["speed", "pong"],
+  categorie: "General",
+  reaction: "📌",
+  use: ".ping",
+}, async (dest, zk, commandeOptions) => {
+  console.log('Command triggered!');
+  const { repondre, ms } = commandeOptions;
+  try {
+    const njabulox = [
+      "https://files.catbox.moe/mh36c7.jpg",
+      "https://files.catbox.moe/bnb3vx.jpg"
+    ];
+
+    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
+    if (!randomNjabulourl) {
+      console.error("Error: No image URL found.");
+      repondre("An error occurred: No image URL found.");
+      return;
+    }
+
+    const reactionEmojis = ['❄️'];
+    const textEmojis = ['🚀'];
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    const runtime = function (seconds) {
+      seconds = Number(seconds);
+      var d = Math.floor(seconds / (3600 * 24));
+      var h = Math.floor((seconds % (3600 * 24)) / 3600);
+      var m = Math.floor((seconds % 3600) / 60);
+      var s = Math.floor(seconds % 60);
+      var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : "";
+      var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : "";
+      var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : "";
+      var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : "";
+      return dDisplay + hDisplay + mDisplay + sDisplay;
+    };
+
+    const start = new Date().getTime();
+    await zk.sendPresenceUpdate('composing', dest);
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const cards = [
+      {
+        header: {
+          title: `📊 Uptime`,
+          hasMediaAttachment: true,
+          imageMessage: (await generateWAMessageContent({ image: { url: randomNjabulourl } }, { upload: zk.waUploadToServer })).imageMessage,
+        },
+        body: {
+          text: `⏳ *sʏsᴛᴇᴍs ᴜᴘᴛɪᴍᴇ* : *${runtime(process.uptime())} ${reactionEmoji}* `,
+        },
+        footer: {
+          text: "Pσɯҽɾ Ⴆყ Ɲנαвυʟσ Jbᯤ",
+        },
+        nativeFlowMessage: {
+          buttons: [
+            {
+              name: "cta_url",
+              buttonParamsJson: JSON.stringify({
+                display_text: "🌐 View on Channel",
+                url: config.GURL
+              }),
+            },
+            {
+              name: "cta_copy",
+              buttonParamsJson: JSON.stringify({
+                display_text: "📋 Copy Link",
+                copy_code: config.GURL
+              }),
+            },
+          ],
+        },
+      },
+      {
+        header: {
+          title: `📊 Ping`,
+          hasMediaAttachment: true,
+          imageMessage: (await generateWAMessageContent({ image: { url: randomNjabulourl } }, { upload: zk.waUploadToServer })).imageMessage,
+        },
+        body: {
+          text: `⏳ *ping* : *${responseTime.toFixed(2)}s ${reactionEmoji}* `,
+        },
+        footer: {
+          text: "Pσɯҽɾ Ⴆყ Ɲנαвυʟσ Jbᯤ",
+        },
+        nativeFlowMessage: {
+          buttons: [
+            {
+              name: "cta_url",
+              buttonParamsJson: JSON.stringify({
+                display_text: "🌐 View on Channel",
+                url: config.GURL
+              }),
+            },
+            {
+              name: "cta_copy",
+              buttonParamsJson: JSON.stringify({
+                display_text: "📋 Copy Link",
+                copy_code: config.GURL
+              }),
+            },
+          ],
+        },
+      },
+    ];
+
+    const message = generateWAMessageFromContent(
+      dest,
+      {
+        viewOnceMessage: {
+          message: {
+            messageContextInfo: {
+              deviceListMetadata: {},
+              deviceListMetadataVersion: 2,
+            },
+            interactiveMessage: {
+              header: { text: `🔍 System Info` },
+              body: { text: `*📂 sʏsᴛᴇᴍs ʟᴏᴀᴅɪɴɢ*` },
+              carouselMessage: { cards },
+            },
+          },
+        },
+      },
+      { quoted: ms }
+    );
+    await zk.relayMessage(dest, message.message, { messageId: message.key.id });
+  } catch (e) {
+    console.error("Error in menu command:", e);
+    repondre(`An error occurred: ${e.message}`);
+  }
+});
+
+
+ 
