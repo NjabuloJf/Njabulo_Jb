027a60e7bac14ff9bbceb9ca01c5b3e79a4153f6
Njabulo Jb <fanajb65@gmail.com>
2025-12-29 21:36:20 +0200

    Update group-cotrol.js

--- commandes/group-cotrol.js
+++ commandes/group-cotrol.js
@@ -1,16 +1,16 @@
-:
+
 const { exec } = require("child_process");
-const { ezra } = require("../fredi/ezra");
+const { fana } = require("../njabulo/fana");
 const { Sticker, StickerTypes } = require("wa-sticker-formatter");
-const { ajouterOuMettreAJourJid, mettreAJourAction, verifierEtatJid } = require('../luckydatabase/antilien');
-const { atbajouterOuMettreAJourJid, atbverifierEtatJid } = require('../luckydatabase/antibot');
+const { ajouterOuMettreAJourJid, mettreAJourAction, verifierEtatJid } = require('../bdd/antilien');
+const { atbajouterOuMettreAJourJid, atbverifierEtatJid } = require('../bdd/antibot');
 const { search, download } = require('aptoide-scraper');
 const fs = require('fs-extra');
 const conf = require("../set");
 const { default: axios } = require("axios");
 const { getBinaryNodeChild, getBinaryNodeChildren } = require("@whiskeysockets/baileys")['default'];
 
-ezra({ 
+fana({ 
   'nomCom': 'approve', 
   'aliases': ["approve-all", "accept"], 
   'categorie': "Group", 
@@ -36,7 +36,7 @@ ezra({
   await zk.sendMessage(origineMessage, { text: "All pending participants have been approved to join by Lucky md." }, { quoted: ms });
 });
 
-ezra({ 
+fana({ 
   'nomCom': "vcf", 
   'aliases': ["savecontact", "savecontacts"], 
   'categorie': "Group", 
@@ -76,7 +76,7 @@ ezra({
   }
 });
 
-ezra({ 
+fana({ 
   'nomCom': 'invite', 
   'aliases': ["link"], 
   'categorie': 'Group', 
@@ -97,7 +97,7 @@ ezra({
   }
 });
 
-ezra({ 
+fana({ 
   'nomCom': 'revoke', 
   'categorie': 'Group' 
 }, async (origineMessage, zk, commandeOptions) => {
@@ -115,111 +115,4 @@ ezra({
 
 
 
-ezra({ 
-  'nomCom': "antiword", 
-  'categorie': 'Group', 
-  'reaction': '🔗' 
-}, async (origineMessage, zk, commandeOptions) => {
-  var { repondre, arg, verifGroupe, superUser, verifAdmin, ms } = commandeOptions;
-  if (!verifGroupe) {
-    await zk.sendMessage(origineMessage, { text: "*This command is for groups only*" }, { quoted: ms });
-    return;
-  }
-  if (superUser || verifAdmin) {
-    const antiwordStatus = await verifierEtatJid(origineMessage);
-    try {
-      if (!arg || !arg[0] || arg === " ") {
-        await zk.sendMessage(origineMessage, { text: "antiword on to activate the anti-word feature\nantiword off to deactivate the anti-word feature\nantiword action/remove to directly remove the sender without notice\nantiword action/warn to give warnings\nantiword action/delete to remove the word without any sanctions\n\nPlease note that by default, the anti-word feature is set to delete." }, { quoted: ms });
-        return;
-      };
-      if (arg[0] === 'on') {
-        if (antiwordStatus) {
-          await zk.sendMessage(origineMessage, { text: "the antiword is already activated for this group" }, { quoted: ms });
-        } else {
-          await ajouterOuMettreAJourJid(origineMessage, "oui");
-          await zk.sendMessage(origineMessage, { text: "the antiword is activated successfully" }, { quoted: ms });
-        }
-      } else {
-        if (arg[0] === "off") {
-          if (antiwordStatus) {
-            await ajouterOuMettreAJourJid(origineMessage, "non");
-            await zk.sendMessage(origineMessage, { text: "The antiword has been successfully deactivated" }, { quoted: ms });
-          } else {
-            await zk.sendMessage(origineMessage, { text: "antiword is not activated for this group" }, { quoted: ms });
-          }
-        } else {
-          if (arg.join('').split('/')[0] === 'action') {
-            let action = arg.join('').split('/')[1].toLowerCase();
-            if (action == 'remove' || action == "warn" || action == "delete") {
-              await mettreAJourAction(origineMessage, action);
-              await zk.sendMessage(origineMessage, { text: "The anti-word action has been updated to " + arg.join('').split('/')[1] }, { quoted: ms });
-            } else {
-              await zk.sendMessage(origineMessage, { text: "The only actions available are warn, remove, and delete" }, { quoted: ms });
-            }
-          } else {
-            await zk.sendMessage(origineMessage, { text: "antiword on to activate the anti-word feature\nantiword off to deactivate the anti-word feature\nantiword action/remove to directly remove the word sender without notice\nantiword action/warn to give warnings\nantiword action/delete to remove the word without any sanctions\n\nPlease note that by default, the anti-word feature is set to delete." }, { quoted: ms });
-          }
-        }
-      }
-    } catch (error) {
-      await zk.sendMessage(origineMessage, { text: error }, { quoted: ms });
-    }
-  } else {
-    await zk.sendMessage(origineMessage, { text: "You are not entitled to this order" }, { quoted: ms });
-  }
-});
-
-ezra({ 
-  'nomCom': "antilink-all", 
-  'categorie': "Group", 
-  'reaction': '🕯️' 
-}, async (origineMessage, zk, commandeOptions) => {
-  const { repondre, arg, verifGroupe, superUser, verifAdmin, ms } = commandeOptions;
-  if (!verifGroupe) {
-    await zk.sendMessage(origineMessage, { text: "*This Command works in Groups Only*" }, { quoted: ms });
-    return;
-  }
-  if (superUser || verifAdmin) {
-    const antilinkStatus = await verifierEtatJid(origineMessage);
-    try {
-      if (!arg || !arg[0].trim()) {
-        await zk.sendMessage(origineMessage, { text: "Type `antilink-all on` to activate the antilink-all feature\nor `antilink-all off` to deactivate the antilink-all feature\nThen `antilink-all action/remove` to directly remove the link without notice\nor `antilink-all action/warn` to give warnings\nor `antilink-all action/delete` to remove the link without any sanctions\n\nPlease note that by default, the antilink-all feature is set to delete." }, { quoted: ms });
-        return;
-      }
-      const [action, subAction] = arg.join(" ").split('/');
-      if (action === 'on') {
-        if (antilinkStatus) {
-          await zk.sendMessage(origineMessage, { text: "Antilink-all is already activated for this group." }, { quoted: ms });
-        } else {
-          await ajouterOuMettreAJourJid(origineMessage, "oui");
-          await zk.sendMessage(origineMessage, { text: "The antilink-all feature has been activated successfully." }, { quoted: ms });
-        }
-      } else {
-        if (action === "off") {
-          if (antilinkStatus) {
-            await ajouterOuMettreAJourJid(origineMessage, "non");
-            await zk.sendMessage(origineMessage, { text: "The antilink-all feature has been successfully deactivated." }, { quoted: ms });
-          } else {
-            await zk.sendMessage(origineMessage, { text: "Antilink-all is not activated for this group." }, { quoted: ms });
-          }
-        } else {
-          if (action === 'action') {
-            const subAction = arg.join('').split('/')[1].toLowerCase();
-            if (["remove", "warn", "delete"].includes(subAction)) {
-              await mettreAJourAction(origineMessage, subAction);
-              await zk.sendMessage(origineMessage, { text: "The anti-link action has been updated to " + subAction + '.' }, { quoted: ms });
-            } else {
-              await zk.sendMessage(origineMessage, { text: "The only actions available are `warn`, `remove`, and `delete`." }, { quoted: ms });
-            }
-          } else {
-            await zk.sendMessage(origineMessage, { text: "Type `antilink-all on` to activate the antilink-all feature\nor `antilink-all off` to deactivate the antilink-all feature\nor `antilink-all action/remove` to directly remove the link without notice\nor `antilink-all action/warn` to give warnings\nor `antilink-all action/delete` to remove the link without any sanctions\n\nPlease note that by default, the antilink-all feature is set to delete.\n\n*KEEP USING LUCKY_MD*" }, { quoted: ms });
-          }
-        }
-      }
-    } catch (error) {
-      await zk.sendMessage(origineMessage, { text: "Error: " + error.message }, { quoted: ms });
-    }
-  } else {
-    await zk.sendMessage(origineMessage, { text: "You are not allowed to use this command." }, { quoted: ms });
-  }
-});
+      
