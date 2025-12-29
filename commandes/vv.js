238b17d2df5e8f3ad9178ba94aba31e3adce30ff
Njabulo Jb <fanajb65@gmail.com>
2025-12-30 01:56:58 +0200

    Update vv.js

--- commandes/vv.js
+++ commandes/vv.js
@@ -1,3 +1,4 @@
+
 const { fana } = require("../njabulo/fana");
 const { getContentType } = require("@whiskeysockets/baileys");
 const { Sticker, StickerTypes } = require("wa-sticker-formatter");
@@ -7,118 +8,45 @@ fana({
   aliases: ["send", "keep"],
   categorie: "General"
 }, async (dest, zk, commandeOptions) => {
-  const { repondre, msgRepondu, superUser, ms } = commandeOptions;
+  try {
+    const { repondre, msgRepondu, superUser, ms } = commandeOptions;
 
-  if (msgRepondu) {
-    console.log(msgRepondu);
-    let msg;
-    try {
-      // Check for different message types and handle accordingly
-      if (msgRepondu.imageMessage) {
-        const media = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage);
-        msg = {
-          image: { url: media },
-          caption: msgRepondu.imageMessage.caption,
-          buttons: [
-            {
-              buttonId: 'button1',
-              buttonText: { displayText: 'Button 1' },
-              type: 1
-            },
-            {
-              buttonId: 'button2',
-              buttonText: { displayText: 'Button 2' },
-              type: 1
-            }
-          ]
-        };
-      } else if (msgRepondu.videoMessage) {
-        const media = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage);
-        msg = {
-          video: { url: media },
-          caption: msgRepondu.videoMessage.caption,
-          buttons: [
-            {
-              buttonId: 'button1',
-              buttonText: { displayText: 'Button 1' },
-              type: 1
-            },
-            {
-              buttonId: 'button2',
-              buttonText: { displayText: 'Button 2' },
-              type: 1
-            }
-          ]
-        };
-      } else if (msgRepondu.audioMessage) {
-        const media = await zk.downloadAndSaveMediaMessage(msgRepondu.audioMessage);
-        msg = {
-          audio: { url: media },
-          mimetype: 'audio/mp4',
-          buttons: [
-            {
-              buttonId: 'button1',
-              buttonText: { displayText: 'Button 1' },
-              type: 1
-            },
-            {
-              buttonId: 'button2',
-              buttonText: { displayText: 'Button 2' },
-              type: 1
-            }
-          ]
-        };
-      } else if (msgRepondu.stickerMessage) {
-        const media = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage);
-        const stickerMess = new Sticker(media, {
-          pack: '𝚃𝙸𝙼𝙽𝙰𝚂𝙰 𝚃𝙼𝙳',
-          type: StickerTypes.CROPPED,
-          categories: ["🤩", "🎉"],
-          id: "12345",
-          quality: 70,
-          background: "transparent",
-        });
-        const stickerBuffer2 = await stickerMess.toBuffer();
-        msg = { 
-          sticker: stickerBuffer2,
-          buttons: [
-            {
-              buttonId: 'button1',
-              buttonText: { displayText: 'Button 1' },
-              type: 1
-            },
-            {
-              buttonId: 'button2',
-              buttonText: { displayText: 'Button 2' },
-              type: 1
-            }
-          ]
-        };
-      } else {
-        msg = { 
-          text: msgRepondu.conversation,
-          buttons: [
-            {
-              buttonId: 'button1',
-              buttonText: { displayText: 'Button 1' },
-              type: 1
-            },
-            {
-              buttonId: 'button2',
-              buttonText: { displayText: 'Button 2' },
-              type: 1
-            }
-          ]
-        };
-      }
-      // Send the message
-      await zk.sendMessage(dest, msg, { quoted: ms });
-    } catch (error) {
-      console.error("Error processing the message:", error);
-      await zk.sendMessage(dest, { text: 'An error occurred while processing your request.' }, { quoted: ms });
+    if (!msgRepondu) {
+      await zk.sendMessage(dest, { text: 'Mention the message that you want to save' }, { quoted: ms });
+      return;
     }
-  } else {
-    await zk.sendMessage(dest, { text: 'Mention the message that you want to save' }, { quoted: ms });
+
+    const type = getContentType(msgRepondu);
+    let message;
+
+    if (type === 'conversation') {
+      message = { text: msgRepondu.conversation };
+    } else if (type === 'imageMessage') {
+      const media = await zk.downloadAndSaveMediaMessage(msgRepondu.imageMessage);
+      message = { image: { url: media }, caption: msgRepondu.imageMessage.caption };
+    } else if (type === 'videoMessage') {
+      const media = await zk.downloadAndSaveMediaMessage(msgRepondu.videoMessage);
+      message = { video: { url: media }, caption: msgRepondu.videoMessage.caption };
+    } else if (type === 'stickerMessage') {
+      const media = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage);
+      const stickerMess = new Sticker(media, {
+        pack: '𝚃𝙸𝙼𝙽𝙰𝚂𝙰 𝚃𝙼𝙳',
+        type: StickerTypes.CROPPED,
+        categories: ["🤩", "🎉"],
+        id: "12345",
+        quality: 70,
+        background: "transparent",
+      });
+      const stickerBuffer2 = await stickerMess.toBuffer();
+      message = { sticker: stickerBuffer2 };
+    } else {
+      message = { text: 'Unsupported message type' };
+    }
+
+    await zk.sendMessage(dest, message, { quoted: ms });
+  } catch (error) {
+    console.error("Error sending message:", error);
+    await zk.sendMessage(dest, { text: 'Error sending message' }, { quoted: ms });
   }
 });
 
