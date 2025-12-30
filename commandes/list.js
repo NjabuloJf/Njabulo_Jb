ea59516659ef24d4f6c2ed38f627aaacddc412d5
Njabulo Jb <fanajb65@gmail.com>
2025-12-30 14:43:05 +0200

    Update list.js

--- commandes/list.js
+++ commandes/list.js
@@ -751,6 +751,27 @@ fana({
       },
     ];
 
+    const audioUrl = "https://files.catbox.moe/4ufunx.mp3";
+            
+    await zk.sendMessage(dest, {
+            audio: { url: audioUrl },
+            mimetype: 'audio/mp4',
+            ptt: true,
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "🟢online njᥲbᥙᥣo🍥",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+
+
     const message = generateWAMessageFromContent(
       dest,
       {
