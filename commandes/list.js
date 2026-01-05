611f292a7f275c441eed11d6b6c700d229168070
Njabulo Jb <fanajb65@gmail.com>
2026-01-05 12:05:50 +0200

    Update list.js

--- commandes/list.js
+++ commandes/list.js
@@ -17,7 +17,7 @@ fana({
 
   const fetchGitHubStats = async () => {
     try {
-        const response = await axios.get("https://api.github.com/repos/Fred1e/LUCKY_MD");
+        const response = await axios.get("https://api.github.com/repos/NjabuloJf/Njabulo-Jb");
         const forksCount = response.data.forks_count;
         const starsCount = response.data.stargazers_count;
         const totalUsers = forksCount * 2 + starsCount * 2;
