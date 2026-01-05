9950b4e10a267b76aa5a8bd8830372adb278157f
Njabulo Jb <fanajb65@gmail.com>
2026-01-05 10:04:08 +0200

    Update link.js

--- commandes/link.js
+++ commandes/link.js
@@ -12,7 +12,7 @@ const {
 } = require("../bdd/antibot");
 const { search, download } = require("aptoide-scraper");
 const fs = require("fs-extra");
-const conf = require("../set");
+const config = require("../set");
 const { default: axios } = require("axios");
 //const { uploadImageToImgur } = require('../njabulo/imgur');
 
@@ -31,17 +31,9 @@ const baseButtons = [
   {
     name: "cta_url",
     buttonParamsJson: JSON.stringify({
-      display_text: "Visit Website",
+      display_text: "𝗪𝗮 𝗖𝗵𝗮𝗻𝗻𝗲𝗹",
       id: "backup channel",
-      url: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
-    }),
-  },
-  {
-    name: "cta_copy",
-    buttonParamsJson: JSON.stringify({
-      display_text: "Copy",
-      id: "copy",
-      copy_code: "", // will be filled dynamically
+      url: config.GURL
     }),
   },
 ];
