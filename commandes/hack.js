15841ab9c78743eb843d003cf6bc9f4d3be0709e
Njabulo Jb <fanajb65@gmail.com>
2026-01-05 09:58:53 +0200

    Update hack.js

--- commandes/hack.js
+++ commandes/hack.js
@@ -1,4 +1,5 @@
 const { fana } = require("../njabulo/fana");
+const config = require("../set");
 
 // ── Random image list ─────────────────────────────────────────────
 const njabulox = [
@@ -15,17 +16,9 @@ const baseButtons = [
   {
     name: "cta_url",
     buttonParamsJson: JSON.stringify({
-      display_text: "Visit Website",
+      display_text: "𝗪𝗮 𝗖𝗵𝗮𝗻𝗻𝗲𝗹",
       id: "backup channel",
-      url: "https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u",
-    }),
-  },
-  {
-    name: "cta_copy",
-    buttonParamsJson: JSON.stringify({
-      display_text: "Copy",
-      id: "copy",
-      copy_code: "", // will be filled dynamically
+      url: config.GURL
     }),
   },
 ];
