78dddc4b679c014bdb11d0cbf74a7dd093378dcb
Njabulo Jb <fanajb65@gmail.com>
2026-01-16 23:32:46 +0200

    Update gpp.js

--- commandes/gpp.js
+++ commandes/gpp.js
@@ -1,106 +1,70 @@
+
+
+Here's the updated code:
 const { fana } = require("../njabulo/fana");
 const moment = require("moment-timezone");
-
-    // List of image URLs
-    const njabulox = [
-        "https://files.catbox.moe/iii5jv.jpg",
-        "https://files.catbox.moe/xjeyjh.jpg",
-        "https://files.catbox.moe/mh36c7.jpg",
-        "https://files.catbox.moe/u6v5ir.jpg",
-        "https://files.catbox.moe/bnb3vx.jpg" // New image added
-    ];
-
-    // Select a random image file
-    const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
-
-  moment.tz.setDefault("Africa/Botswana");
-  const temps = moment().format('HH:mm:ss');
-  const date = moment().format('DD/MM/YYYY');
-
-    
+// List of image URLs
+const njabulox = [
+  "https://files.catbox.moe/iii5jv.jpg",
+  "https://files.catbox.moe/xjeyjh.jpg",
+  "https://files.catbox.moe/mh36c7.jpg",
+  "https://files.catbox.moe/u6v5ir.jpg",
+  "https://files.catbox.moe/bnb3vx.jpg" // New image added
+];
+// Select a random image file
+const randomNjabulourl = njabulox[Math.floor(Math.random() * njabulox.length)];
+moment.tz.setDefault("Africa/Botswana");
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
 
 async function sendFormattedMessage(zk, chatId, text, ms) {
   await zk.sendMessage(chatId, {
-      interactiveMessage: {
-        header: {
-          documentMessage: {
-            url: randomNjabulourl,
-            mimetype: 'image/jpeg',
-            fileSha256: '',
-            fileLength: '',
-            pageCount: 0,
-            mediaKey: '',
-            fileName: 'FEE-XMD MENU',
-            fileEncSha256: '',
-            directPath: '',
-            mediaKeyTimestamp: '',
-            jpegThumbnail: '',
+    interactiveMessage: {
+      header: {
+        documentMessage: {
+          url: randomNjabulourl,
+          mimetype: 'image/jpeg',
+          fileSha256: '',
+          fileLength: '',
+          pageCount: 0,
+          mediaKey: '',
+          fileName: 'FEE-XMD MENU',
+          fileEncSha256: '',
+          directPath: '',
+          mediaKeyTimestamp: '',
+          jpegThumbnail: '',
+        },
+        hasMediaAttachment: true,
+      },
+      image: { url: randomNjabulourl },
+      header: text,
+      footer: `Pσɯҽɾҽԃ Ⴆყ njabulo`,
+      nativeFlowMessage: {
+        buttons: [
+          {
+            name: 'cta_url',
+            buttonParamsJson: JSON.stringify({
+              display_text: '📢 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡',
+              url: 'https://whatsapp.com/channel/0029Vb6mzVF7tkj42VNPrZ3V',
+              merchant_url: 'https://whatsapp.com/channel/0029Vb6mzVF7tkj42VNPrZ3V',
+            }),
           },
-          hasMediaAttachment: true,
-        },
-        image: { url: randomNjabulourl },
-       header: text,
-        footer: `Pσɯҽɾҽԃ Ⴆყ njabulo`,
-        nativeFlowMessage: {
-          buttons: [
-            {
-              name: 'cta_url',
-              buttonParamsJson: JSON.stringify({
-                display_text: '📢 𝙊𝙛𝙛𝙞𝙘𝙞𝙖𝙡 𝘾𝙝𝙖𝙣𝙣𝙚𝙡',
-                url: 'https://whatsapp.com/channel/0029Vb6mzVF7tkj42VNPrZ3V',
-                merchant_url: 'https://whatsapp.com/channel/0029Vb6mzVF7tkj42VNPrZ3V',
-              }),
-            },
-            {
-              name: 'single_select',
-              buttonParamsJson: JSON.stringify({
-                title: '𝐕𝐈𝐄𝐖☇ 𝐎𝐏𝐓𝐈𝐎𝐍𝐒 ☑',
-                sections: [
-                  {
-                    title: '⌜ 𝘾𝙤𝙧𝙚 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨 ⌟',
-                    highlight_label: '© 丨几匚',
-                    rows: [
-                      { title: '𝐏𝐢𝐧𝐠', description: 'Check bot response time', id: `ping` },
-                      { title: '𝐑𝐞𝐩𝐨', description: 'Get bot repository link', id: `repo` },
-                    ],
-                  },
-                  {
-                    title: 'ℹ 𝙄𝙣𝙛𝙤 𝘽𝙤𝙩',
-                    highlight_label: '© 丨几匚',
-                    rows: [
-                      { title: '𝐒𝐞𝐭𝐭𝐢𝐧𝐠𝐬', description: 'Show bot settings', id: `settings` },
-                      { title: '𝐒𝐮𝐩𝐩𝐨𝐫𝐭', description: 'Get support information', id: `support` },
-                    ],
-                  },
-                  {
-                    title: '📜 𝘾𝙖𝙩𝙚𝙜𝙤𝙧𝙮 𝙈𝙚𝙣𝙪𝙨',
-                    highlight_label: '© 丨几匚',
-                    rows: [
-                      { title: '𝐆𝐞𝐧𝐞𝐫𝐚𝐥𝐌𝐞𝐧𝐮', description: 'General commands', id: `generalmenu` },
-                      { title: '𝐒𝐞𝐭𝐭𝐢𝐧𝐠𝐬𝐌𝐞𝐧𝐮', description: 'Bot settings commands', id: `settingsmenu` },
-                      { title: '𝐬𝐢𝐧𝐞𝐬𝐬𝐌𝐞𝐧𝐮', description: 'Bot Currency exchange commands', id: `businessmenu` },
-                    ],
-                  },
-                ],
-              }),
-            },
-          ],
-          messageParamsJson: JSON.stringify({
-            limited_time_offer: {
-              text: 'FEE-XMD',
-              url: 'https://github.com/Fred1e/Fee-Xmd',
-              copy_code: 'FREDI',
-              expiration_time: moment().add(1, 'hour').valueOf(),
-            },
-            bottom_sheet: {
-              in_thread_buttons_limit: 2,
-              divider_indices: [1, 2],
-              list_title: 'Select Command',
-              button_title: 'FEE-XMD MENU',
-            },
-          }),
-        },
-      },     
+        ],
+        messageParamsJson: JSON.stringify({
+          limited_time_offer: {
+            text: 'FEE-XMD',
+            url: 'https://github.com/Fred1e/Fee-Xmd',
+            copy_code: 'FREDI',
+            expiration_time: moment().add(1, 'hour').valueOf(),
+          },
+          bottom_sheet: {
+            in_thread_buttons_limit: 2,
+            divider_indices: [1, 2],
+            list_title: 'Select Command',
+            button_title: 'FEE-XMD MENU',
+          },
+        }),
+      },
       contextInfo: {
         externalAdReply: {
           title: `⏰ message menu`,
@@ -110,8 +74,9 @@ async function sendFormattedMessage(zk, chatId, text, ms) {
           renderLargerThumbnail: true,
         },
       },
-    }, { quoted: ms });
-
+    },
+  }, { quoted: ms });
+}
 
 fana(
   {
@@ -121,14 +86,11 @@ fana(
   },
   async (chatId, zk, commandeOptions) => {
     const { ms, repondre, msgRepondu, auteurMsgRepondu, mybotpic, nomAuteurMessage } = commandeOptions;
-
     if (!msgRepondu) {
       return sendFormattedMessage(zk, chatId, `Yo ${nomAuteurMessage}, reply to someone’s message to snag their profile pic! 😡 Don’t make Njabulo Jb do extra work! 🤔`, ms);
     }
-
     try {
       await sendFormattedMessage(zk, chatId, `Yo ${nomAuteurMessage}, Njabulo Jb’s hunting for @${auteurMsgRepondu.split("@")[0]}’s profile pic! 📸 Hold tight! 🔍`, ms);
-
       let ppuser;
       try {
         ppuser = await zk.profilePictureUrl(auteurMsgRepondu, 'image');
@@ -136,32 +98,32 @@ fana(
         ppuser = mybotpic();
         await sendFormattedMessage(zk, chatId, `Yo ${nomAuteurMessage}, @${auteurMsgRepondu.split("@")[0]}’s profile pic is locked tight! 😣 Njabulo Jb’s got you my pic instead! 😎`, ms);
       }
-
       await zk.sendMessage(
         chatId,
         {
           image: { url: ppuser },
           caption: `BOOM, ${nomAuteurMessage}! Snagged @${auteurMsgRepondu.split("@")[0]}’s profile pic! 🔥`,
           mentions: [auteurMsgRepondu],
-            }, { quoted: {
+        },
+        {
+          quoted: {
             key: {
-                fromMe: false,
-                participant: `0@s.whatsapp.net`,
-                remoteJid: "status@broadcast"
+              fromMe: false,
+              participant: `0@s.whatsapp.net`,
+              remoteJid: "status@broadcast"
             },
             message: {
-                contactMessage: {
-                    displayName: "NנɐႦυℓσ נႦ✆︎",
-                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
-                }
+              contactMessage: {
+                displayName: "NנɐႦυℓσ נႦ✆︎",
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD`
+              }
             }
-        } });
-    }
-
+          }
+        }
+      );
     } catch (error) {
       console.error("Error in .getpp command:", error);
       await sendFormattedMessage(zk, chatId, `TOTAL BUST, ${nomAuteurMessage}! Njabulo Jb crashed while grabbing the pic: ${error.message} 😡 Try again or flop! 😣`, ms);
     }
   }
 );
-
