1935aff64347578edcab74b4cac8f361a351b7a1
Njabulo Jb <fanajb65@gmail.com>
2026-02-03 20:12:15 +0200

    Update antilien.js

--- bdd/antilien.js
+++ bdd/antilien.js
@@ -1,5 +1,5 @@
 
-require("dotenv").config();
+require 'dotenv').config();
 const { Pool } = require("pg");
 
 const dbUrl = process.env.DATABASE_URL;
@@ -18,11 +18,14 @@ const proConfig = {
 
 const pool = new Pool(proConfig);
 
-// Fonction pour créer la table "antilien"
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
 async function createAntilienTable() {
   const client = await pool.connect();
   try {
-    // Exécutez une requête SQL pour créer la table "antilien" si elle n'existe pas déjà
     await client.query(`
       CREATE TABLE IF NOT EXISTS antilien (
         jid text PRIMARY KEY,
@@ -38,20 +41,16 @@ async function createAntilienTable() {
   }
 }
 
-// Appelez la méthode pour créer la table "antilien"
 createAntilienTable();
 
 async function ajouterOuMettreAJourJid(jid, etat) {
   const client = await pool.connect();
   try {
-    // Vérifiez si le jid existe déjà dans la table 'antilien'
     const result = await client.query('SELECT * FROM antilien WHERE jid = $1', [jid]);
     const jidExiste = result.rows.length > 0;
     if (jidExiste) {
-      // Si le jid existe, mettez à jour l'état avec la valeur passée en argument
       await client.query('UPDATE antilien SET etat = $1 WHERE jid = $2', [etat, jid]);
     } else {
-      // Si le jid n'existe pas, ajoutez-le avec l'état passé en argument et l'action 'supp' par défaut
       await client.query('INSERT INTO antilien (jid, etat, action) VALUES ($1, $2, $3)', [jid, etat, 'supp']);
     }
     console.log(`JID ${jid} ajouté ou mis à jour avec succès dans la table 'antilien'.`);
@@ -65,14 +64,11 @@ async function ajouterOuMettreAJourJid(jid, etat) {
 async function mettreAJourAction(jid, action) {
   const client = await pool.connect();
   try {
-    // Vérifiez si le jid existe déjà dans la table 'antilien'
     const result = await client.query('SELECT * FROM antilien WHERE jid = $1', [jid]);
     const jidExiste = result.rows.length > 0;
     if (jidExiste) {
-      // Si le jid existe, mettez à jour l'action avec la valeur fournie (et laissez l'état inchangé)
       await client.query('UPDATE antilien SET action = $1 WHERE jid = $2', [action, jid]);
     } else {
-      // Si le jid n'existe pas, ajoutez-le avec l'état 'non' par défaut et l'action fournie
       await client.query('INSERT INTO antilien (jid, etat, action) VALUES ($1, $2, $3)', [jid, 'non', action]);
     }
     console.log(`Action mise à jour avec succès pour le JID ${jid} dans la table 'antilien'.`);
@@ -86,13 +82,11 @@ async function mettreAJourAction(jid, action) {
 async function verifierEtatJid(jid) {
   const client = await pool.connect();
   try {
-    // Recherchez le JID dans la table 'antilien' et récupérez son état
     const result = await client.query('SELECT etat FROM antilien WHERE jid = $1', [jid]);
     if (result.rows.length > 0) {
       const etat = result.rows[0].etat;
       return etat === 'oui';
     } else {
-      // Si le JID n'existe pas dans la table, il n'est pas enregistré comme "oui"
       return false;
     }
   } catch (error) {
@@ -106,18 +100,16 @@ async function verifierEtatJid(jid) {
 async function recupererActionJid(jid) {
   const client = await pool.connect();
   try {
-    // Recherchez le JID dans la table 'antilien' et récupérez son action
     const result = await client.query('SELECT action FROM antilien WHERE jid = $1', [jid]);
     if (result.rows.length > 0) {
       const action = result.rows[0].action;
       return action;
     } else {
-      // Si le JID n'existe pas dans la table, retournez une valeur par défaut (par exemple, 'supp')
       return 'supp';
     }
   } catch (error) {
     console.error('Erreur lors de la récupération de l\'action du JID dans la table :', error);
-    return 'supp'; // Gestion de l'erreur en retournant une valeur par défaut
+    return 'supp';
   } finally {
     client.release();
   }
@@ -129,5 +121,3 @@ module.exports = {
   verifierEtatJid,
   recupererActionJid,
 };
-
-
