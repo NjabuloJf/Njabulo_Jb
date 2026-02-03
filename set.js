017e9e1bec8b44efece1136c79806d2dab43073a
Njabulo Jb <fanajb65@gmail.com>
2026-02-03 20:48:59 +0200

    Update set.js

--- set.js
+++ set.js
@@ -1,5 +1,4 @@
-
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/set.env' });
 const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
 
@@ -10,30 +9,32 @@ if (!DATABASE_URL || !DATABASE_URL.startsWith('postgres://')) {
   process.exit(1);
 }
 
-module.exports = {
+const config = {
   session: process.env.SESSION_ID || 'zokk',
   PREFIXE: process.env.PREFIX || ".",
   OWNER_NAME: process.env.OWNER_NAME || "Njabulo Jb",
-  NUMERO_OWNER : process.env.NUMERO_OWNER || "26777821911",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "26777821911",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
-  AUTO_BIO : process.env.AUTO_BIO || 'yes',
-  AUTOREACT_STATUS : process.env.AUTOREACT_STATUS || 'yes',
+  AUTO_BIO: process.env.AUTO_BIO || 'yes',
+  AUTOREACT_STATUS: process.env.AUTOREACT_STATUS || 'yes',
   AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-  BOT : process.env.BOT_NAME || 'Njabulo-Jb',
-  URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/mh36c7.jpg',
+  BOT: process.env.BOT_NAME || 'Njabulo-Jb',
+  URL: process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/mh36c7.jpg',
   GURL: process.env.GURL || 'https://whatsapp.com/channel/0029VbAckOZ7tkj92um4KN3u',
   MODE: process.env.PUBLIC_MODE || "yes",
   PM_PERMIT: process.env.PM_PERMIT || 'yes',
-  HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
-  HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
-  WARN_COUNT : process.env.WARN_COUNT || '3' ,
-  ETAT : process.env.PRESENCE || '',
-  CHATBOT : process.env.PM_CHATBOT || 'no',
-  DP : process.env.STARTING_BOT_MESSAGE || "yes",
-  ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+  HEROKU_APY_KEY: process.env.HEROKU_APY_KEY,
+  WARN_COUNT: process.env.WARN_COUNT || '3',
+  ETAT: process.env.PRESENCE || '',
+  CHATBOT: process.env.PM_CHATBOT || 'no',
+  DP: process.env.STARTING_BOT_MESSAGE || "yes",
+  ADM: process.env.ANTI_DELETE_MESSAGE || 'no',
   DATABASE_URL,
   DATABASE: DATABASE_URL,
 };
+
+module.exports = config;
 
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
@@ -41,5 +42,5 @@ fs.watchFile(fichier, () => {
   console.log(`mise à jour ${__filename}`);
   delete require.cache[fichier];
   require(fichier);
-})
+});
 
